53b05a6fca02022257df399c7ddf065f20bdf113
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2020-03-09 19:32:58 -0400

    Initial commit

---
+++ resources/views/Clients/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>    
+  <form class="form-inline">    
+      Informe seu Email
+      <input type="text" readonly class="form-control-plaintext" id="mail">
+      <label for="inputPassword2" class="sr-only">Senha</label>
+      <input type="password" class="form-control" id="password" placeholder="Digite sua senha">
+    <button type="submit" class="btn btn-primary mb-2">Cadastrar</button>
+  </form>
+</body>
+</html>
+
